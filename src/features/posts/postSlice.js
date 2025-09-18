bc5875161e4b562be52754ba23ccbc998b6df9bd
penghs <52020193+penghs520@users.noreply.github.com>
2025-09-18 23:45:40 +0000

    Initial commit

---
+++ src/features/posts/postSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initState = {
+
+}
+
+const postSlice = createSlice({
+    name: "posts",
+    initialState: initState,
+    reducers: {
+
+    }
+})
+
+export default postSlice.reducer;
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import postReducer from './features/posts/postSlice'
+
+export default configureStore(
+    {
+        reducer: {
+            posts : postReducer
+        }
+    }
+)
